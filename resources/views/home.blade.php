1400a6c925128a117bbdc9880bee383d2a27fefb
Vanio Anibal Macamo <macamovanioanibal@gmail.com>
2023-12-15 14:22:28 +0200

    Feature: Adicionei o adminLTE ao Projecto.

---
+++ resources/views/home.blade.php
@@ -0,0 +1,23 @@
+<!-- resources/views/home.blade.php -->
+
+@extends('adminlte::page')
+
+@section('content_header')
+    <h1>Dashboard</h1>
+@stop
+
+@section('content')
+    <div class="row">
+        <div class="col-md-12">
+            <div class="box">
+                <div class="box-header with-border">
+                   
+                </div>
+                <div class="box-body">
+                    <!-- Conteúdo da sua página vai aqui -->
+                    <p>Esta é a página inicial do seu aplicativo.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+@stop
