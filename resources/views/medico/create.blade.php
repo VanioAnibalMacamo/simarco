e7881d05af6d44c118f2ad0ae0863e57e8cb43c6
Vanio Anibal Macamo <macamovanioanibal@gmail.com>
2024-06-18 19:15:24 +0200

    #Improvement: Adicionei o campo imagem na view

--- resources/views/medico/create.blade.php
+++ resources/views/medico/create.blade.php
@@ -22,7 +22,6 @@
                     <input type="text" class="form-control" id="nome" name='nome' placeholder="Digite o nome do Medico...">
                 </div>
                 <div class="form-row">
-                    
                     <div class="form-group col-md-4">
                         <label for="especialidade_id">Especialidade</label>
                         <select class="form-control" id="especialidade_id" name="especialidade_id">
@@ -32,33 +31,34 @@
                             @endforeach
                         </select>
                     </div>
-                    
-                    
                     <div class="form-group col-md-4">
                         <label for="numero_identificacao">Número de Identificação</label>
                         <input type="text" class="form-control" id="numero_identificacao" name='numero_identificacao' placeholder="Ex: 123456789">
                     </div>
-                
-                
-                <div class="form-group col-md-4">
-                    <label for="disponibilidade">Disponibilidade</label>
-                    <select class="form-control" id="disponibilidade" name="disponibilidade">
-                        <option value="">Selecione a Disponibilidade</option>
-                        @foreach ($disponibilidades as $value)
-                            <option value="{{ $value }}">{{ ucfirst($value) }}</option>
-                        @endforeach
-                    </select>
+                    <div class="form-group col-md-4">
+                        <label for="disponibilidade">Disponibilidade</label>
+                        <select class="form-control" id="disponibilidade" name="disponibilidade">
+                            <option value="">Selecione a Disponibilidade</option>
+                            @foreach ($disponibilidades as $value)
+                                <option value="{{ $value }}">{{ ucfirst($value) }}</option>
+                            @endforeach
+                        </select>
+                    </div>
+                    <div class="form-group col-md-4">
+                        <label for="genero">Gênero</label>
+                        <select class="form-control" id="genero" name="genero">
+                            <option value="">Selecione um Gênero</option>
+                            @foreach ($generos as $value)
+                                <option value="{{ $value }}">{{ ucfirst($value) }}</option>
+                            @endforeach
+                        </select>
+                    </div>
+                    <div class="form-group col-md-4">
+                        <label for="imagem">Imagem</label>
+                        <input type="file" class="form-control-file" id="imagem" name="imagem" onchange="previewImagem(this)">
+                        <img id="preview-imagem" src="#" alt="Preview da Imagem" style="display: none; max-width: 100px; margin-top: 10px;">
+                    </div>
                 </div>
-                <div class="form-group col-md-4">
-                    <label for="genero">Gênero</label>
-                    <select class="form-control" id="genero" name="genero">
-                        <option value="">Selecione um Gênero</option>
-                        @foreach ($generos as $value)
-                            <option value="{{ $value }}">{{ ucfirst($value) }}</option>
-                        @endforeach
-                    </select>
-                </div>
-            </div>
             </div>
             <div class="card-footer">
                 <input type="submit" class="btn btn-primary" value='Salvar'>
@@ -74,5 +74,20 @@
 @stop
 
 @section('js')
-    <script> console.log('Hi!'); </script>
+    <script>
+        function previewImagem(input) {
+            var preview = document.getElementById('preview-imagem');
+            if (input.files && input.files[0]) {
+                var reader = new FileReader();
+                reader.onload = function (e) {
+                    preview.src = e.target.result;
+                    preview.style.display = 'block';
+                }
+                reader.readAsDataURL(input.files[0]);
+            } else {
+                preview.src = '#';
+                preview.style.display = 'none';
+            }
+        }
+    </script>
 @stop
