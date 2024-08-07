fc8dab2599f19b83c8291a8f1105ff26d4d157dd
Vânio Anibal  Macamo <macamovanioanibal@gmail.com>
2024-08-07 20:02:16 +0200

    #Feature: Migration empresa criada

---
+++ database/migrations/2024_08_07_180112_create_empresas_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::create('empresas', function (Blueprint $table) {
+            $table->id();
+            $table->string('nome');
+            $table->string('sigla')->nullable();
+            $table->string('nuit')->unique();
+            $table->string('email')->unique();
+            $table->string('contacto1');
+            $table->string('contacto2')->nullable();
+            $table->string('localizacao')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('empresas');
+    }
+};
